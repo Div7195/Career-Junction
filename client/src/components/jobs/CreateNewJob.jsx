9d45d5b3ea26d6d52ee5da3993540c31d21a409d
N1ckool <104719138+Div7195@users.noreply.github.com>
2023-12-14 00:51:05 +0530

    made layout of createjob

--- client/src/components/jobs/CreateNewJob.jsx
+++ client/src/components/jobs/CreateNewJob.jsx
@@ -1,9 +1,316 @@
-
-
+import { TextField } from "@mui/material";
+import { DemoContainer } from '@mui/x-date-pickers/internals/demo';
+import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
+import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
+import { DatePicker } from '@mui/x-date-pickers/DatePicker';
+import InputLabel from '@mui/material/InputLabel';
+import FormControl from '@mui/material/FormControl';
+import NativeSelect from '@mui/material/NativeSelect';
+import skills from "../../constants/skills.js";
+import { cleanDigitSectionValue } from "@mui/x-date-pickers/internals/hooks/useField/useField.utils";
 const CreateNewJob = () => {
+    
     return(
-        <div>
+        
+        <div style={{
+            marginTop:'64px',
+            display:'flex',
+            justifyContent:'center',
+            fontSize:'20px',
+            color:'black'
 
+        }}>
+            <div style={{
+                display:'flex',
+                justifyContent:'center',
+                flexDirection:'column',
+                marginTop:'10px',
+                fontSize:'15px'
+                
+            }}>
+            <div style={{
+                display:'flex',
+                flexDirection:'column',
+                marginTop:'15px'
+            }}>
+                <div style={{
+                    color:'black'
+                }}>
+                    Job Title
+                </div>
+
+                <div>
+                    <TextField
+                        id="filled-multiline-flexible"
+                        label="Multiline"
+                        multiline
+                        maxRows={4}
+                        variant="filled"
+                            style={{width:400}}
+                        />
+                </div>
+            </div>
+
+            <div style={{
+                display:'flex',
+                flexDirection:'column',
+                marginTop:'15px'
+            }}>
+                <div style={{
+                    color:'black'
+                }}>
+                    Job Type
+                </div>
+
+                <div>
+                    <TextField
+                        id="filled-multiline-flexible"
+                        label="Multiline"
+                        multiline
+                        maxRows={4}
+                        variant="filled"
+                        style={{width:400}}
+                        />
+                </div>
+            </div>
+            <div style={{
+                display:'flex',
+                flexDirection:'column',
+                marginTop:'15px'
+            }}>
+                <div style={{
+                    color:'black'
+                }}>
+                    Stipend
+                </div>
+
+                <div>
+                    <TextField
+                        id="filled-multiline-flexible"
+                        label="Multiline"
+                        multiline
+                        maxRows={4}
+                        variant="filled"
+                        style={{width:400}}
+                        />
+                </div>
+            </div>
+            <div style={{
+                display:'flex',
+                flexDirection:'column',
+                marginTop:'15px'
+            }}>
+                <div style={{
+                    color:'black'
+                }}>
+                    Salary
+                </div>
+
+                <div>
+                    <TextField
+                        id="filled-multiline-flexible"
+                        label="Multiline"
+                        multiline
+                        maxRows={4}
+                        variant="filled"
+                        style={{width:400}}
+                        />
+                </div>
+            </div>
+            <div style={{
+                display:'flex',
+                flexDirection:'column',
+                marginTop:'15px'
+            }}>
+                <div style={{
+                    color:'black'
+                }}>
+                    Duration
+                </div>
+
+                <div>
+                    <TextField
+                        id="filled-multiline-flexible"
+                        label="Multiline"
+                        multiline
+                        maxRows={4}
+                        variant="filled"
+                        style={{width:400}}
+                        />
+                </div>
+            </div>
+            <div style={{
+                display:'flex',
+                flexDirection:'column',
+                marginTop:'15px'
+            }}>
+                <div style={{
+                    color:'black'
+                }}>
+                    Location
+                </div>
+
+                <div>
+                    <TextField
+                        id="filled-multiline-flexible"
+                        label="Multiline"
+                        multiline
+                        maxRows={4}
+                        variant="filled"
+                        style={{width:400}}
+                        />
+                </div>
+            </div>
+            <div style={{
+                display:'flex',
+                flexDirection:'column',
+                marginTop:'15px'
+            }}>
+                <div style={{
+                    color:'black'
+                }}>
+                    Start Date
+                </div>
+
+                <div>
+                
+                <LocalizationProvider dateAdapter={AdapterDayjs} >
+                <DemoContainer components={['DatePicker']} >
+                <DatePicker label="Basic date picker" />
+                </DemoContainer>
+                </LocalizationProvider>
+                    
+                </div>
+            </div>
+            <div style={{
+                display:'flex',
+                flexDirection:'column',
+                marginTop:'15px'
+            }}>
+                <div style={{
+                    color:'black'
+                }}>
+                    Skills Required
+                </div>
+
+                <div>
+                <FormControl fullWidth>
+                    
+                    <NativeSelect
+                        defaultValue={30}
+                        inputProps={{
+                        name: 'age',
+                        id: 'uncontrolled-native',
+                        }}
+                    >
+                    {
+                        skills.map((skill) =>
+                        (
+                            
+                            <option value={skill.name}>{skill.name}</option>
+                        ))
+                    }
+                        
+                    </NativeSelect>
+                </FormControl>
+                </div>
+            </div>
+            <div style={{
+                display:'flex',
+                flexDirection:'column',
+                marginTop:'15px'
+            }}>
+                <div style={{
+                    color:'black'
+                }}>
+                    Work Hours
+                </div>
+
+                <div>
+                    <TextField
+                        id="filled-multiline-flexible"
+                        label="Multiline"
+                        multiline
+                        maxRows={4}
+                        variant="filled"
+                        style={{width:400}}
+                        />
+                </div>
+            </div>
+            <div style={{
+                display:'flex',
+                flexDirection:'column',
+                marginTop:'15px'
+            }}>
+                <div style={{
+                    color:'black'
+                }}>
+                    Job requiredments
+                </div>
+
+                <div>
+                    <TextField
+                        id="filled-multiline-flexible"
+                        label="Multiline"
+                        multiline
+                        maxRows={4}
+                        variant="filled"
+                        style={{width:400}}
+                        />
+                </div>
+            </div>
+            <div style={{
+                display:'flex',
+                flexDirection:'column',
+                marginTop:'15px'
+            }}>
+                <div style={{
+                    color:'black'
+                }}>
+                    Responsibilities
+                </div>
+
+                <div>
+                    <TextField
+                        id="filled-multiline-flexible"
+                        label="Multiline"
+                        multiline
+                        maxRows={4}
+                        variant="filled"
+                        style={{width:400}}
+                        />
+                </div>
+            </div>
+            <div style={{
+                display:'flex',
+                flexDirection:'column',
+                marginTop:'15px'
+            }}>
+                <div style={{
+                    color:'black'
+                }}>
+                    Hiring Process
+                </div>
+
+                <div>
+                    <TextField
+                        id="filled-multiline-flexible"
+                        label="Multiline"
+                        multiline
+                        maxRows={4}
+                        variant="filled"
+                        style={{width:400}}
+                        />
+                </div>
+            </div>
+            
+
+
+        </div>
+            
+        
+            
+        
         </div>
     );
 }
---
+++ client/src/constants/skills.js
@@ -0,0 +1,40 @@
+const skills = [
+    { type: 'IT', name: 'JavaScript' },
+    { type: 'IT', name: 'Python' },
+    { type: 'IT', name: 'Java' },
+    { type: 'IT', name: 'C#' },
+    { type: 'IT', name: 'Ruby' },
+    // ... Add more IT skills
+  
+    { type: 'Tech Frameworks', name: 'React' },
+    { type: 'Tech Frameworks', name: 'Angular' },
+    { type: 'Tech Frameworks', name: 'Vue.js' },
+    { type: 'Tech Frameworks', name: 'Django' },
+    { type: 'Tech Frameworks', name: 'Ruby on Rails' },
+    // ... Add more tech frameworks
+  
+    { type: 'Languages', name: 'English' },
+    { type: 'Languages', name: 'Spanish' },
+    { type: 'Languages', name: 'French' },
+    { type: 'Languages', name: 'Chinese' },
+    { type: 'Languages', name: 'German' },
+    // ... Add more languages
+  
+    { type: 'Finance', name: 'Accounting' },
+    { type: 'Finance', name: 'Financial Analysis' },
+    { type: 'Finance', name: 'Budgeting' },
+    // ... Add more finance-related skills
+  
+    { type: 'Accounts', name: 'Bookkeeping' },
+    { type: 'Accounts', name: 'Accounts Receivable' },
+    { type: 'Accounts', name: 'Accounts Payable' },
+    // ... Add more accounting skills
+  
+    { type: 'Consultancy', name: 'Management Consulting' },
+    { type: 'Consultancy', name: 'Business Strategy' },
+    { type: 'Consultancy', name: 'Project Management' },
+    // ... Add more consultancy skills
+  
+    // ... Add more categories and skills as needed
+  ];
+  export default skills;
