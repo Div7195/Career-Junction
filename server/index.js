8b6697c0bbd586f385354a466440c5267871158e
N1ckool <104719138+Div7195@users.noreply.github.com>
2024-01-15 14:30:04 +0530

    minor change in server index file

--- server/index.js
+++ server/index.js
@@ -19,7 +19,7 @@ const PASSWORD = process.env.DB_PASSWORD;
 const server = app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
 const io = new Server(server, {
     cors: {
-      origin: 'http://localhost:3000',
+      origin: 'https://career-junction-app.vercel.app',
       methods: ['GET', 'POST'],
     },
   });
