7eb9ea83212017258c540a82e7c827db5d169234
N1ckool <104719138+Div7195@users.noreply.github.com>
2024-01-17 17:23:34 +0530

    dubug attempt continued for socketio

--- server/index.js
+++ server/index.js
@@ -25,7 +25,7 @@ const PASSWORD = process.env.DB_PASSWORD;
 const server = app.listen(PORT, () => console.log(`server is running on port ${PORT}`));
 const io = new Server(server, {
   cors: {
-    origin: 'https://career-junction-app.vercel.app',
+    origin: '*',
     methods: ['GET', 'POST'],
   },
   });
